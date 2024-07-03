f616370c022095a0c14390e55b953a3bca93a87f
abhiatigre <abhiatigre007@gmail.com>
2024-07-03 17:55:58 +0530

    add - video player & responsive design

---
+++ lib/video_player_screen.dart
@@ -0,0 +1,150 @@
+import 'package:flick_video_player/flick_video_player.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({super.key});
+
+  @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late FlickManager flickManager;
+  String titleName = "";
+
+  final List url = [
+    {
+      "videoUrl":
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+      "videoName": "First video",
+      "image":
+          "https://images.pexels.com/photos/461940/pexels-photo-461940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+      "videoUrl":
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+      "videoName": "Second video",
+      "image":
+          "https://images.pexels.com/photos/461940/pexels-photo-461940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+      "videoUrl":
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+      "videoName": "Third video",
+      "image":
+          "https://images.pexels.com/photos/461940/pexels-photo-461940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+      "videoUrl":
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+      "videoName": "Fourth video",
+      "image":
+          "https://images.pexels.com/photos/461940/pexels-photo-461940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+      "videoUrl":
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+      "videoName": "Fifth video",
+      "image":
+          "https://images.pexels.com/photos/461940/pexels-photo-461940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    }
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    flickManager = FlickManager(
+      // ignore: deprecated_member_use
+      videoPlayerController: VideoPlayerController.network(url[0]["videoUrl"]),
+    );
+
+    titleName = url[0]["videoName"];
+  }
+
+  @override
+  void dispose() {
+    flickManager.dispose();
+    super.dispose();
+  }
+
+  void _playVideo(String videoUrl) {
+    setState(() {
+      // ignore: deprecated_member_use
+      flickManager.handleChangeVideo(VideoPlayerController.network(videoUrl));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Video player"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              elevation: 2,
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: FlickVideoPlayer(flickManager: flickManager),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      titleName,
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              itemCount: url.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  child: Card(
+                    elevation: 1,
+                    color: Colors.white,
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          leading: Image.network(
+                            url[index]["image"],
+                            height: 50,
+                            width: 50,
+                            fit: BoxFit.cover,
+                          ),
+                          title: Text(
+                            url[index]["videoName"],
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        )),
+                  ),
+                  onTap: () {
+                    _playVideo(url[index]["videoUrl"]);
+                    setState(() {
+                      titleName = url[index]["videoName"];
+                    });
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
