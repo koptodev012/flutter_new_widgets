0440a3bbb6b643ab110a3296502c37f50265cc62
abhiatigre <abhiatigre007@gmail.com>
2024-07-05 10:37:28 +0530

    add - navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:responsive_design/presentation/custom_bottom_sheet/home_scree.dart';
 import 'package:responsive_design/presentation/display_chart/model/student_attendenceModel.dart';
 import 'package:responsive_design/presentation/display_chart/student_attendence_Tracker_screen.dart';
+import 'package:responsive_design/presentation/draggable_container/draggable_screen.dart';
+import 'package:responsive_design/presentation/navigation_bar/navigation_bar_screen.dart';
+import 'package:responsive_design/presentation/snackbar/snackbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,14 +22,20 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: StudentAttendenceTrackerScreen(
-        students: [
-          StudentAttendanceModel(name: 'Alice', attended: 8, missed: 2),
-          StudentAttendanceModel(name: 'Bob', attended: 7, missed: 3),
-          StudentAttendanceModel(name: 'Charlie', attended: 9, missed: 1),
-          // Add more students as needed
-        ],
-      ),
+      home: NavigationBarScreen(),
+
+      //! ------------------- for using StudentAttendenceTrackerScreen ----------------
+
+      //  StudentAttendenceTrackerScreen(
+      //   students: [
+      //     StudentAttendanceModel(name: 'Alice', attended: 8, missed: 2),
+      //     StudentAttendanceModel(name: 'Bob', attended: 7, missed: 3),
+      //     StudentAttendanceModel(name: 'Charlie', attended: 9, missed: 1),
+      //     // Add more students as needed
+      //   ],
+      // ),
+
+      //! -------------------------------------------------------------------------------
     );
   }
 }
